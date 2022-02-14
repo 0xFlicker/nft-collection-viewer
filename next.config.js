6f19e9c6e9f29f2a981b345ee7fc21cfa7687bc6
CmdrNFT <CmdrNFT@protonmail.com>
2022-02-14 00:21:54 -0600

    random

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = withTM({
   basePath: isProd ? `/${env.NEXT_GITHUB_PAGES_REPO}` : '',
   assetPrefix: isProd ? `/${env.NEXT_GITHUB_PAGES_REPO}/` : '',
   images: {
-    domains: [new URL(env.NEXT_IPFS_GATEWAY).hostname, ...process.env.NEXT_GITHUB_PAGES_HOST ? [process.env.NEXT_GITHUB_PAGES_HOST] : []],
+    domains: [new URL(env.NEXT_IPFS_GATEWAY).hostname, ...process.env.NEXT_GITHUB_PAGES_HOST ? [new URL(process.env.NEXT_GITHUB_PAGES_HOST).hostname] : []],
   },
   env
 });
